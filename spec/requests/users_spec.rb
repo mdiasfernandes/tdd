24da7300e0679a7e9590201d39f880b77d3b31a6
Matheus Fernandes <matheus@MacBook-Air-de-Admin.local>
2020-03-02 19:04:53 -0300

    Starting controller

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+    describe "GET /Users" do
+        it "Some test" do
+            expect(some_thing)
+        end
+    end
+end
